e92097f2db5151783364d5f7789c10160198c5db
Fadri Furrer <fadri.furrer@dealini.ch>
2013-09-09 16:54:58 +0200

    added exclude flag for lightboxs in admin.py

--- pinry/admin.py
+++ pinry/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from pinry.core.models import LightBox, Image, Pin
 
 class LightBoxAdmin(admin.ModelAdmin):
-    list_display = ('title', 'link', 'admin_image')
+    list_display = ('title', 'link', 'admin_image', 'exclude')
 
 
 class ImageAdmin(admin.ModelAdmin):
